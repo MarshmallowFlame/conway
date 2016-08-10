f1e607b7c5f24333638089fdfa886817d32ffc3c
Svante Rollenhagen <svante.rollenhagen@gmail.com>
2016-08-10 08:52:28 +0200

    Changes to improve speed, as well as loop through seeds to find candidates
    Removed waiting time that really slowed down the program.. Also removed
    the use of deepcopy by moving out the making of new board to another
    function, as well as some other changes

--- conway.py
+++ conway.py
@@ -12,7 +12,7 @@ import copy
 import time
 import math
 #returns number of alive neighbours of given cell
-def neig(r,c):
+def neig(r,c,board):
     global dim
     n=0
     if c>0:
@@ -34,7 +34,7 @@ def neig(r,c):
     return n
 
 #returns string of the current board (with * for alive and . for dead)
-def printBoard():
+def printBoard(board):
     global dim
     theString=""
     for x in range(dim):
@@ -48,30 +48,47 @@ def printBoard():
     return theString
 
 #run the simulation, can be either with "press enter to continue" or a given amount of times
-def runLife(pr,nr):
-	
-    global board
+def iterate(board):
+    global dim
+    newBoard=[[0 for x in range(dim)] for y in range(dim)]
+    for x in range(dim):
+        for y in range(dim):
+            ne = neig(y,x,board)
+            if ne < 2 or ne >3:
+                newBoard[y][x]=0
+            elif ne ==3:
+                #print(str(x)+" "+str(y))
+                newBoard[y][x]=1
+
+        if board == newBoard:
+            return newBoard,0
+            
+
+    return newBoard,1
+
+
+
+
+    
+def runLife(pr,nr,board):
     korv=0
     while korv <=nr:
         if(pr):
-            print(printBoard())
-        newBoard=copy.deepcopy(board)
-        for x in range(dim):
-            for y in range(dim):
-                ne = neig(y,x)
-                if ne < 2 or ne >3:
-                    newBoard[y][x]=0
-                elif ne ==3:
-                    #print(str(x)+" "+str(y))
-                    newBoard[y][x]=1
-        if board == newBoard:
-            print("STILLASTÅENDE")
-            return
-        board=copy.deepcopy(newBoard)
-        time.sleep(0.2)
+            print(printBoard(board))
+            
+        board,err =iterate(board)
+        if not err:
+            if pr:
+                print("SAMMA")
+            return 0
+        
+       
+
+        if pr:
+            time.sleep(0.2)
         #in=input("Press Enter to continue, enter 'q' to quit...")
         korv+=1
-
+    return 1
         
 def getFromFile():
     global dim
@@ -104,14 +121,12 @@ def saveToFile(bo,filename):
             myfile.write("\n")
             
 
-def boardFromSeed(seed):
+def boardFromSeed(seed, sideLength):
     global dim
     
     seed="{0:b}".format(seed)
 
-    dim=1
-    while dim**2<len(seed):
-        dim+=1
+    dim=sideLength
 
     
     if not len(seed)==dim**2:
@@ -131,11 +146,25 @@ def boardFromSeed(seed):
         x+=1
     
     return board
-            
-        
 
+         
+def findUsefulSeeds(sideLength):
+    succ=[]
+    global dim
+    dim = sideLength
+    
+    start = 0
+    end = 2**((dim)**2)
+    print(end)
+    
+    for i in range(end):
+        board = boardFromSeed(i,dim)
+        if runLife(0,100,board):
+            succ.append(i)
+    print(len(succ))
+    return succ    
 
+seedToUse = findUsefulSeeds(10)[500]
 
-board = boardFromSeed(511)
-runLife(1,100)
-saveToFile(board,"seeds/end.cw")
+runLife(1,100,boardFromSeed(seedToUse,4))
+
